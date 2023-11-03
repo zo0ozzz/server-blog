ab2952510660c6635d475c631da651c0d1dde8ca
ungbi <zo0qzzz@gmail.com>
2023-11-03 15:36:44 +0900

    기기 변경

--- posts.js
+++ posts.js
@@ -8,49 +8,24 @@ router.use(function timeLog(req, res, next) {
   next();
 });
 
-router.use(async (req, res, next) => {
-  // const db = await getDB();
-  // req.db = db;
-
-  next();
-});
-
 router.get("/", async (req, res, next) => {
-  const db = await getDB();
-  const col = db.collection("post");
+  try {
+    const db = await getDB();
+    const col = db.collection("post");
 
-  const queryString = req.query.category;
+    const posts = await col.find().toArray();
 
-  if (!queryString) {
-    try {
-      const posts = await col.find().toArray();
-
-      res.send(posts);
-    } catch (error) {
-      console.log(error);
-    }
-  } else {
-    console.log("쿼리스트링 있음", queryString);
-    try {
-      const posts = await col.find({ category: queryString }).toArray();
-      console.log(posts);
-
-      res.send(posts);
-    } catch (error) {
-      console.log(error);
-    }
+    res.send(posts);
+  } catch (error) {
+    console.log(error);
   }
 });
 
 router.get("/:_id", async (req, res, next) => {
-  const db = await getDB();
-
-  console.log("get, /post/_id");
-
   try {
     const _id = parseInt(req.params._id);
-    console.log(_id);
 
+    const db = await getDB();
     const col = db.collection("post");
 
     const post = await col.findOne({ _id: _id });
@@ -61,10 +36,67 @@ router.get("/:_id", async (req, res, next) => {
   }
 });
 
-// router.get("/category", async (req, res, next) => {
-//   const searchString = req.query.category;
-//   console.log(searchString);
-// });
+router.get("/categories/:selectedCategory", async (req, res, next) => {
+  try {
+    const selectedCategory = req.params.selectedCategory;
+
+    const db = await getDB();
+    const col = db.collection("post");
+
+    const posts = await col.find({ category: selectedCategory }).toArray();
+
+    res.send(posts);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/search", async (req, res, next) => {
+  try {
+    const searchString = req.query.searchString;
+    console.log(searchString);
+
+    const db = await getDB();
+    const col = db.collection("post");
+
+    // const query = [
+    //   {
+    //     $search: {
+    //       index: "titleAndContentIndex",
+    //       text: {
+    //         query: searchString,
+    //         path: ["title", "content"],
+    //       },
+    //     },
+    //   },
+    // ];
+    // - search index가 적용된 검색의 쿼리는 배열로 넘겨줘야 함.
+    // - $search 필드 외에 다른 필드로 세부 설정을 하기 때문(인 듯함).
+
+    // const posts = await col.aggregate(query).toArray();
+    // console.log(posts);
+
+    const query = {
+      $or: [
+        { title: { $regex: searchString, $options: "i" } },
+        { content: { $regex: searchString, $options: "i" } },
+      ],
+    };
+    // - $or는 여러 필드 중 하나에서만 해당 조건을 만족해도 서치 결과값으로 넘겨줌.
+    // - 복수의 검색될 필드는 객체들을 담은 배열로 작성.
+    // - $regex는 찾는 조건을 정규식으로 지정한다는 것.
+    // - 이렇게 하면 전체든 부분이든 주어진 단어가 들어가는 모든 걸 검색할 수는 있는데
+    // - 대상을 하나하나 다 순회해야 해서 리소스가 많이 듦.
+
+    const posts = await col.find(query).toArray();
+    console.log(posts);
+
+    res.status(200).send(posts);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 router.post("/", async (req, res, next) => {
   try {
--- server.js
+++ server.js
@@ -36,52 +36,6 @@ app.get("/create", async (req, res, next) => {
   }
 });
 
-app.get("/search", async (req, res, next) => {
-  try {
-    const db = await getDB();
-    const col = db.collection("post");
-
-    const searchString = req.query.searchString;
-
-    // const query = [
-    //   {
-    //     $search: {
-    //       index: "titleAndContentIndex",
-    //       text: {
-    //         query: searchString,
-    //         path: ["title", "content"],
-    //       },
-    //     },
-    //   },
-    // ];
-    // - search index가 적용된 검색의 쿼리는 배열로 넘겨줘야 함.
-    // - $search 필드 외에 다른 필드로 세부 설정을 하기 때문(인 듯함).
-
-    // const posts = await col.aggregate(query).toArray();
-    // console.log(posts);
-
-    const query = {
-      $or: [
-        { title: { $regex: searchString, $options: "i" } },
-        { content: { $regex: searchString, $options: "i" } },
-      ],
-    };
-    // - $or는 여러 필드 중 하나에서만 해당 조건을 만족해도 서치 결과값으로 넘겨줌.
-    // - 복수의 검색될 필드는 객체들을 담은 배열로 작성.
-    // - $regex는 찾는 조건을 정규식으로 지정한다는 것.
-    // - 이렇게 하면 전체든 부분이든 주어진 단어가 들어가는 모든 걸 검색할 수는 있는데
-    // - 대상을 하나하나 다 순회해야 해서 리소스가 많이 듦.
-
-    const posts = await col.find(query).toArray();
-    console.log(posts);
-
-    res.status(200).send(posts);
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
-
 app.delete("/deleteAllData", async (req, res, next) => {
   try {
     const db = await getDB();
